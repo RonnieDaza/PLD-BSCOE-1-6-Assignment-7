82a5f5a7d512b936d4192eabd102365577fab5e9
unknown <daza.ronnie.09212002@gmail.com>
2021-11-29 14:00:47 +0800

    Import re Commit

---
+++ Program 2 - Password Validator.py
@@ -0,0 +1,15 @@
+# Program 2 - Password Validator
+# Create a program that checks if the password is valid.
+# The password is valid if all criteria are met:
+# a. Greater than 15 letters
+# b. Have at least one capital letter
+# c. Have at least one number
+# d. Have at least one special character (!@#$%^&*()_+ etc)
+# Example:
+# Input:
+# P@ssword+P@ssword13
+# Output: Valid
+
+# Steps
+# 1. Import re.
+import re
