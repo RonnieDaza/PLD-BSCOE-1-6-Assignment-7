1cca3ffeea72e027590c37a6ab2d910d94e97f1b
unknown <daza.ronnie.09212002@gmail.com>
2021-11-29 13:29:56 +0800

    Ask the user for the desired sentence Commit

---
+++ Program 1 - Word, Vowel, and Consonant.py
@@ -0,0 +1,13 @@
+# Program 1 - Word, Vowel, and Consonant Counter
+# Create a program that asks for a sentence as input.
+# Display the number of words, vowels, and consonants in the input.
+# Example:
+# Input: Bahala kayo dyan
+# Output:
+# Words: 3
+# Vowels: 6
+# Consonant: 8
+
+# Steps
+# 1. Ask the user for the desired sentence.
+userSentence = input("Enter your desired sentece: ")
